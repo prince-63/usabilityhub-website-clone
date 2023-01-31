dbb41cd24c3933d6fcc10218a45ce34f72ece5a5
Prince Kumar <princekumarprasad12345@gmail.com>
2023-01-31 14:37:22 +0530

    landing page complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import logo from "../../assets/asset 1.png";
 
+function clicked() {
+    const links = document.getElementById("links");
+    if(links.style.display === "none") {
+        links.style.display = "block";
+    }else {
+        links.style.display = "none";
+    }
+}
+
 export default function Header() {
     return (
         <>
@@ -8,7 +17,7 @@ export default function Header() {
                 <div className="logo">
                     <img src={logo} alt="this is a logo" />
                 </div>
-                <div className="links">
+                <div className="links" id="links">
                     <ul>
                         <li><a href={"/"}>Product <i className="fa-solid fa-chevron-down"></i></a></li>
                         <li><a href={"/"}>Customers</a></li>
@@ -17,6 +26,9 @@ export default function Header() {
                         <li><a href={"/"} className="first">Sign in</a></li>
                         <li><a href={"/"} className="second">Sign up</a></li>
                     </ul>
+                </div>
+                <div id="menu_section">
+                    <i className="fa-solid fa-bars" onClick={clicked}></i>
                 </div>
             </div>
         </>
